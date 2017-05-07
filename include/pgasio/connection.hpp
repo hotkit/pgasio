ab884c664e4af4fe309d03db59c68e13f9687fda
Kirit Sælensminde <k@kirit.com>
2017-05-07 15:51:23 +0700

    Sketch out relationship between handshake and connection.

--- include/pgasio/connection.hpp
+++ include/pgasio/connection.hpp
@@ -12,5 +12,32 @@
 namespace pgasio {
 
 
+    template<typename S>
+    class connection;
+
+
+    /// Perform a handshake that assumes to authentication or connection
+    /// options are needed.
+    template<typename S> inline
+    connection<S> handshake(S &&socket, boost::asio::yield_context &yield) {
+        return connection<S>(std::move(socket));
+    }
+
+
+    /// The connection to the database
+    template<typename S>
+    class connection {
+        friend connection<S> handshake<>(S &&, boost::asio::yield_context &);
+
+        S socket;
+
+        connection(S &&s)
+        : socket(std::move(s)) {
+        }
+
+    public:
+    };
+
+
 }
 
