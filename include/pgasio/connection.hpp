b1f602202bd465ba9a901a3ec34854fad198d8e0
Kirit Sælensminde <k@kirit.com>
2017-05-17 18:17:10 +0700

    Handle unsecured connections.

--- include/pgasio/connection.hpp
+++ include/pgasio/connection.hpp
@@ -35,7 +35,32 @@ namespace pgasio {
         cmd.int8(0);
         cmd.send(socket, yield);
         std::unordered_map<std::string, std::string> settings;
-        return connection<S>(std::move(socket), std::move(settings));
+        int32_t process_id{}, secret{};
+        while ( socket.is_open() ) {
+            auto header = packet_header(socket, yield);
+            auto body = header.packet_body(socket, yield);
+            decoder decode = byte_view(body);
+            switch ( header.type ) {
+            case 'K':
+                process_id = decode.read_int32();
+                secret = decode.read_int32();
+                break;
+            case 'R':
+                break; // Do nothing, the authentication was succesful
+            case 'S': {
+                    auto name = decode.read_string();
+                    auto value = decode.read_string();
+                    settings.insert(std::make_pair(std::move(name), std::move(value)));
+                }
+                break;
+            case 'Z':
+                return connection<S>(std::move(socket), std::move(settings), process_id, secret);
+            default:
+                throw std::runtime_error(
+                    std::string("Unknown connection packet type: ") + header.type);
+            }
+        }
+        throw std::runtime_error("The Postgres connection closed during the initial handshake");
     }
 
 
@@ -44,14 +69,15 @@ namespace pgasio {
     class connection {
         friend connection<S> handshake<>(S, const char *, const char *, boost::asio::yield_context &);
 
-        std::unordered_map<std::string, std::string> settings;
-
-        connection(S s, std::unordered_map<std::string, std::string> set)
-        : settings(std::move(set)), socket(std::move(s)) {
+        connection(S s, std::unordered_map<std::string, std::string> set, int32_t pid, int32_t sec)
+        : socket(std::move(s)), settings(std::move(set)), process_id(pid), secret(sec) {
         }
 
     public:
         S socket;
+        const std::unordered_map<std::string, std::string> settings;
+        const int32_t process_id;
+        const int32_t secret;
     };
 
 
